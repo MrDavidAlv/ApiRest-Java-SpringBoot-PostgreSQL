396d624d03eb22c60bf650b88a16e438aec3f3bc
mrdavidalv <davidalvarez33@hotmail.com>
2026-01-28 11:10:14 -0500

    cors

--- src/main/java/com/litethinking/enterprise/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/litethinking/enterprise/infrastructure/security/SecurityConfig.java
@@ -49,9 +49,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
         configuration.setExposedHeaders(List.of("Authorization"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
